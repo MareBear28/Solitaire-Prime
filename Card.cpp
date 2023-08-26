d62b54bf9fc8529b646cc22391b003d7c4185dd7
MareBear28 <marielurbano28@gmail.com>
2023-08-26 16:31:25 -0700

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,10 +1,3 @@
-// Mariel Urbano
-// Class (CECS 282-01)
-// Project Name (Prog 1 – Solitaire Prime)
-// Due Date (9/13/2021)
-//
-// I certify that this program is my own original work. I did not copy any part of this program from
-// any other source. I further certify that I typed each and every line of code in this program
 #include "Card.h"
 #include <iostream>
 #include <string>
